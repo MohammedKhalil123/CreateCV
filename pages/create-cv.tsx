057d3bb309e9d5313aa8c482ed5a7f9610f4a22b
mohammed <mohammed1997316@gmail.com>
2022-09-26 08:50:54 +0300

    added go back and go next functions

--- pages/create-cv.tsx
+++ pages/create-cv.tsx
@@ -2,15 +2,28 @@ import CVPreview from "../components/create cv/cv_preview";
 import styles from "../components/create cv/create_cv.module.css";
 import CVForm from "../components/create cv/cv_form";
 import Head from "next/head";
+import { useState } from "react";
 
 const CreateCVPage = () => {
+  const [index, setIndex] = useState<number>(0);
+
+  const goBack = () => {
+    setIndex(0);
+  };
+
+  const goNext = () => {
+    setIndex(1);
+  };
+
   return (
     <main className={styles.main}>
       <Head>
         <title>Create your CV</title>
       </Head>
-      <CVForm />
-      <CVPreview />
+
+      {index === 0 ? <CVForm /> : null}
+
+      {index === 0 ? <CVPreview /> : null}
     </main>
   );
 };
