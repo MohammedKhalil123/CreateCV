fdfcf27725b31cacd4bb876e8373319d9a91553a
mohammedabu-arida <m.k.abuarida@gmail.com>
2022-10-06 17:55:55 +0300

    working on adding cv preview

--- components/create cv/cv_preview.tsx
+++ components/create cv/cv_preview.tsx
@@ -1,10 +1,13 @@
 import { Page, PDFViewer, Document } from "@react-pdf/renderer";
+import { CVInformation } from "./cv_form";
 import styles from "./cv_preview.module.css";
 
-export default function CVPreview() {
+export default function CVPreview(cvInfo: CVInformation) {
   return (
     <div className={styles.pageContainer}>
-      <section className={styles.page}></section>
+      <section className={styles.page}>
+        <div>{cvInfo.personalInformation.firstName}</div>
+      </section>
     </div>
   );
 }
